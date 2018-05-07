c2edf2a8a44c280f36cf8306b8cfd2165092ea7f
Daniel Witek <dwitek.mmt-b2015@fh-salzburg.ac.at>
2018-05-07 07:42:34 +0200

    Task 2

--- Task 2/README.md
+++ Task 2/README.md
@@ -1,9 +1,10 @@
-#  Semantic Web Task 1
+#  Semantic Web Task 2
 
 ## How to use
 
 1. Move to project folder
-2. In console, type `php inverted.php`
+2. Default folder for `.txt` files is `corpus`, if named otherwise, please specify in `tf-idf.php` through variable `$folder_name`
+2. In console, type `php tf-idf.php`
 3. Follow instructions
 
 ## Basic queries
--- Task 2/tf-idf.php
+++ Task 2/tf-idf.php
@@ -1,8 +1,11 @@
 <?php
-
+ini_set ('memory_limit', '1024M');
 $dictionary = array();
+$folder_name = "corpus";
+$num_files = count(glob("$folder_name/*.txt"));
 
-foreach (glob("ressources/*.txt") as $filename) {
+foreach (glob("$folder_name/*.txt") as $filename) {
+
     $text = file_get_contents($filename, "r");
     $text = strtolower(preg_replace("/\n\r|\n|\r/"," ", $text));
 
@@ -12,39 +15,71 @@ foreach (glob("ressources/*.txt") as $filename) {
       {
         if (!array_key_exists($filename, $dictionary[$word]))
         {
-          //$dictionary[$word]["documentcount"]? $dictionary[$word]["documentcount"]+1 : 1;
-          //var_dump(count($dicitionary[$word]));
-          //$dictionary[$word]=[$filename];
-          //array_push($dictionary[$word], $filename);
-          $dictionary[$word][$filename]=1;
+          $dictionary[$word][$filename] = 1;
         }
         else {
-          $dictionary[$word][$filename]+=1;
+          $dictionary[$word][$filename] += 1;
         }
       }
       else
       {
-        //$dictionary[$word]=[$filename];
-        //$dictionary[$word]["documentcount"]=1;
-        $dictionary[$word][$filename]=1;
+        $dictionary[$word][$filename] = 1;
       }
     }
 }
-ksort($dictionary);
-var_dump($dictionary);
+
+ksort($dictionary);   //nach keywords sortiert
 
 do {
     $query = strtolower(readline("Enter query, leave empty to quit: "));
     $query_array = explode(" ", $query);
-    $intersect = array_intersect_key($dictionary, array_flip($query_array));
-    if (count($intersect)>1) {
-      $result = array_unique(call_user_func_array('array_intersect', $intersect));
-    }
-    else {
-      $result = array_key_exists(reset($query_array), $intersect)? $intersect[reset($query_array)] : array();
-    }
+    $result = array();
+    if ($query !="") {
+    foreach (glob("$folder_name/*.txt") as $filename) {
+        foreach ($query_array as $queryword) {
+          if (array_key_exists($queryword, $dictionary) && array_key_exists($filename, $dictionary[$queryword])){
+            $idf = $num_files/count($dictionary[$queryword]);
+            $tf = $dictionary[$queryword][$filename];
 
-    $resultstring = implode(", ", $result);
-    echo "$resultstring\n";
+            $result[$filename][$queryword] = $idf * $tf;
+
+            if (!array_key_exists("scrnr", $result[$filename])) {
+              $result[$filename]["scrnr"]=$result[$filename][$queryword];
+            }
+            else $result[$filename]["scrnr"]+= $result[$filename][$queryword];
+          }
+        }
+      }
+      if (count($result)>1) {
+
+      uasort($result, function ($file1, $file2) {
+        return $file2['scrnr'] <=> $file1['scrnr'];
+      });
+    }
+      if (count($result) > 5) {
+        $top = "top 5";
+      }
+      elseif (count($result) == 0) {
+        $top = "none, because THERE ARE NONE";
+      }
+      else {
+        $top = "all of them";
+      }
+
+      echo "\nFound ".count($result)." results, showing $top \n";
+      $limit = 0;
+      foreach ($result as $file => $value) {
+        $file = basename($file);
+        echo "\n$file: score: $value[scrnr] \n";
+        foreach ($value as $key => $count) {
+          if ($key != "scrnr") {
+            echo "$key: $count\n";
+          }
+        }
+        if (++$limit == 5) break;     //max 5
+      }
+    }
+    echo "\n";
 } while ($query);
+
 ?>
